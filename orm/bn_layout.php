b146c1f3700a6d5bc7df7db6c15372855de05f20
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-18 10:51:26 -0500

    v.0.11.2 Se integran paquetes, nomina, organigrama, cat_sat, direccion_postal, empleado, im_registro_patronal, documento, facturacion, comercial, xml, proceso

---
+++ orm/bn_layout.php
@@ -0,0 +1,27 @@
+<?php
+namespace models;
+use base\orm\modelo;
+use PDO;
+
+class nom_layout extends modelo{
+
+    public function __construct(PDO $link){
+        $tabla = __CLASS__;
+        $columnas = array($tabla=>false);
+        $campos_obligatorios = array();
+
+        $campos_view = array();
+        $campos_view['bn_sucursal_id']['type'] = 'selects';
+        $campos_view['bn_sucursal_id']['model'] = (new bn_sucursal($link));
+
+        $campos_view['nom_periodo_id']['type'] = 'selects';
+        $campos_view['nom_periodo_id']['model'] = (new nom_periodo($link));
+
+        $campos_view['fecha_pago']['type'] = 'dates';
+
+        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, campos_view: $campos_view);
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+}
