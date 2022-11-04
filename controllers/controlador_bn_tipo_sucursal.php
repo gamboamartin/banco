42c431ed3fa8fc8e0832b1724c3520b543c74fe2
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-11-04 09:49:03 -0600

    v0.19.4 FIX Se ajusto elemento de constructor de controlers

--- controllers/controlador_bn_tipo_sucursal.php
+++ controllers/controlador_bn_tipo_sucursal.php
@@ -40,7 +40,7 @@ class controlador_bn_tipo_sucursal extends system {
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new bn_tipo_sucursal(link: $link);
         $html_ = new bn_tipo_sucursal_html(html: $html);
-        $obj_link = new links_menu($this->registro_id);
+        $obj_link = new links_menu(link: $link, registro_id:link: $link, registro_id:$this->registro_id);
         parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Tipo sucursal';
